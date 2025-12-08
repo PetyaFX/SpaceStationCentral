4aef33eb1d8e24a34f3518b2837ac6be6b05cce8
pe7yu <pety4n@gmail.com>
2025-12-08 17:11:47 +0100

    Update log_handlers.py

--- app/core/log_handlers.py
+++ app/core/log_handlers.py
@@ -62,7 +62,7 @@ class DiscordWebhookHandler(logging.Handler):
             f"- {record.funcName}:{record.lineno}"
         )
 
-    def discord_handler_factory() -> logging.Handler:
+    def discord_handler_factory(self) -> logging.Handler:
         """
         Factory function for creating DiscordWebhookHandler from config.
 
